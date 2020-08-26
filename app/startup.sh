7220369418cc7743b47bf6966038b3673adeb116
harakzf <14cc123f@al.rikkyo.ac.jp>
2020-08-26 23:00:06 +0900

    5_0826

--- app/startup.sh
+++ app/startup.sh
@@ -3,7 +3,7 @@
 cd /var/app
 
 # uwsgi起動
-uwsgi --socket 127.0.0.1:3031 --chdir /var/app/ --wsgi-file app.py --callable app --master --processes 4 --threads 2 & 
+uwsgi --socket 18.179.198.134:3031 --chdir /var/app/ --wsgi-file app.py --callable app --master --processes 4 --threads 2 & 
 
 # nginx起動
 nginx -g "daemon off;"
